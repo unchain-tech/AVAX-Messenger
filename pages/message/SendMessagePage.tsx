3697d7cafd06acda2cd9c48753f58c163d0edaf6
rakiyama <ryojiro25@gmail.com>
2022-08-30 13:55:24 +0900

    use contract

--- pages/message/SendMessagePage.tsx
+++ pages/message/SendMessagePage.tsx
@@ -2,14 +2,51 @@ import { useState } from "react";
 import Layout from "../../components/layout";
 import WalletLayout from "../../components/walletLayout";
 import { useWallet } from "../../hooks/useWallet";
+import { ethers } from "ethers";
+import abi from "../../utils/Messenger.json";
 
-function SendMessage() {
+const contractAddress = "0xC3c90d7093712840c62ef806B1a026377A293286";
+const contractABI = abi.abi;
+
+type Props = {
+  text: string;
+  receiver: string;
+  token: number; // numberでいいのかわからん！
+};
+
+async function SendMessage({ text, receiver, token }: Props) {
   alert("send");
+  try {
+    const { ethereum } = window as any;
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const MessengerContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+      // 予期せぬ処理ブロックなどでガス量が無理にならないための上限を設定
+      // MAX_ETH = gas_fee * gasLimit
+      const postTxn = await MessengerContract.post(text, receiver, {
+        gasLimit: 300000,
+        value: token, // check in wei
+      });
+      console.log("Mining...", postTxn.hash);
+      await postTxn.wait();
+      console.log("Mined -- ", postTxn.hash);
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default function SendMessagePage() {
   const [textValue, setTextValue] = useState("");
   const [tokenValue, setTokenValue] = useState("");
+  const [receiverAccountValue, setReceiverAccountValue] = useState("");
   const { currentAccount } = useWallet();
 
   return (
@@ -18,7 +55,7 @@ export default function SendMessagePage() {
         <div>
           <div>First Page !</div>
           <div>wallet is {currentAccount}</div>
-          {/* 投稿ボックスを実装*/}
+          {/* テキストボックス */}
           {currentAccount && (
             <textarea
               name="messageArea"
@@ -29,6 +66,18 @@ export default function SendMessagePage() {
             />
           )}
           {textValue}
+          {/* 受信者ボックス */}
+          {currentAccount && (
+            <textarea
+              name="messageArea"
+              placeholder="type here"
+              id="content"
+              value={receiverAccountValue}
+              onChange={(e) => setReceiverAccountValue(e.target.value)}
+            />
+          )}
+          {receiverAccountValue}
+          {/* トークンボックス */}
           {currentAccount && (
             <textarea
               name="messageArea"
@@ -41,7 +90,16 @@ export default function SendMessagePage() {
           {parseInt(tokenValue, 10)}
           {/* sendボタンにsend関数を連動 */}
           {currentAccount && (
-            <button className="postButton" onClick={SendMessage}>
+            <button
+              className="postButton"
+              onClick={() => {
+                SendMessage({
+                  text: textValue,
+                  receiver: receiverAccountValue,
+                  token: parseInt(tokenValue, 10),
+                });
+              }}
+            >
               send
             </button>
           )}
@@ -50,30 +108,3 @@ export default function SendMessagePage() {
     </Layout>
   );
 }
-
-//const post = async () => {
-//  try {
-//    const { ethereum } = window;
-//    if (ethereum) {
-//      const provider = new ethers.providers.Web3Provider(ethereum);
-//      const signer = provider.getSigner();
-//      const snsPortalContract = new ethers.Contract(
-//        contractAddress,
-//        contractABI,
-//        signer
-//      );
-//      // 予期せぬ処理ブロックなどでガス量が無理にならないための上限を設定
-//      // MAX_ETH = gas_fee * gasLimit
-//      const postTxn = await snsPortalContract.post(postValue, {
-//        gasLimit: 300000,
-//      });
-//      console.log("Mining...", postTxn.hash);
-//      await postTxn.wait();
-//      console.log("Mined -- ", postTxn.hash);
-//    } else {
-//      console.log("Ethereum object doesn't exist!");
-//    }
-//  } catch (error) {
-//    console.log(error);
-//  }
-//};
